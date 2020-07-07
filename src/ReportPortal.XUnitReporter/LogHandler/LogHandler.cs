1bfa23c38b09b32458aa1936823c6121dd3c8b01
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2020-07-07 18:56:58 +0300

    Use commands approach to handle log messages

--- src/ReportPortal.XUnitReporter/LogHandler/LogHandler.cs
+++ src/ReportPortal.XUnitReporter/LogHandler/LogHandler.cs
@@ -1,8 +1,8 @@
-﻿using ReportPortal.Client.Abstractions.Requests;
-using ReportPortal.Shared;
+﻿using ReportPortal.Shared;
+using ReportPortal.Shared.Execution.Logging;
 using ReportPortal.Shared.Extensibility;
+using ReportPortal.Shared.Extensibility.Commands;
 using ReportPortal.Shared.Internal.Logging;
-using ReportPortal.Shared.Logging;
 using ReportPortal.XUnitReporter.LogHandler.Messages;
 using System;
 using System.Collections.Concurrent;
@@ -13,29 +13,35 @@ using Xunit.Abstractions;
 
 namespace ReportPortal.XUnitReporter.LogHandler
 {
-    public class LogHandler : ILogHandler
+    public class LogHandler : ICommandsListener
     {
         private static readonly ITraceLogger TraceLogger;
 
         private static ConcurrentDictionary<ILogScope, ITestOutputHelper> _outputHelperMap = new ConcurrentDictionary<ILogScope, ITestOutputHelper>();
+
+        public void Initialize(ICommandsSource commandsSource)
+        {
+            commandsSource.OnBeginLogScopeCommand += CommandsSource_OnBeginLogScopeCommand;
+            commandsSource.OnEndLogScopeCommand += CommandsSource_OnEndLogScopeCommand;
+            commandsSource.OnLogMessageCommand += CommandsSource_OnLogMessageCommand;
+        }
 
         static LogHandler()
         {
             var currentDirectory = Path.Combine(Path.GetDirectoryName(new Uri(typeof(ReportPortalReporter).Assembly.Location).LocalPath));
 
             TraceLogger = TraceLogManager.Instance.WithBaseDir(currentDirectory).GetLogger<LogHandler>();
-
         }
 
         public static ITestOutputHelper XunitTestOutputHelper
         {
             get
             {
-                return _outputHelperMap[Log.RootScope];
+                return _outputHelperMap[Context.Current.Log.Root];
             }
             set
             {
-                var rootScope = Log.RootScope;
+                var rootScope = Context.Current.Log.Root;
 
                 TraceLogger.Verbose($"Fixture is helping to assign ITestOutputHelper with current root scope {rootScope.GetHashCode()}...");
 
@@ -43,48 +49,18 @@ namespace ReportPortal.XUnitReporter.LogHandler
             }
         }
 
-        public int Order => 10;
-
-        public void BeginScope(ILogScope logScope)
+        private void CommandsSource_OnLogMessageCommand(Shared.Execution.ILogContext logContext, Shared.Extensibility.Commands.CommandArgs.LogMessageCommandArgs args)
         {
-            if (_outputHelperMap.TryGetValue(Log.RootScope, out ITestOutputHelper output))
-            {
-                var communicationMessage = new BeginScopeCommunicationMessage
-                {
-                    Id = logScope.Id,
-                    ParentScopeId = logScope.Parent?.Id,
-                    Name = logScope.Name,
-                    BeginTime = logScope.BeginTime
-                };
-
-                //output.WriteLine(Client.Converters.ModelSerializer.Serialize<BeginScopeCommunicationMessage>(communicationMessage));
-                NotifyAgent(output, Client.Converters.ModelSerializer.Serialize<BeginScopeCommunicationMessage>(communicationMessage));
-            }
-        }
-
-        public void EndScope(ILogScope logScope)
-        {
-            var communicationMessage = new EndScopeCommunicationMessage
-            {
-                Id = logScope.Id,
-                EndTime = logScope.EndTime.Value,
-                Status = logScope.Status
-            };
-
-            var outputHelper = _outputHelperMap[Log.RootScope];
-
-            //outputHelper.WriteLine(Client.Converters.ModelSerializer.Serialize<EndScopeCommunicationMessage>(communicationMessage));
-            NotifyAgent(outputHelper, Client.Converters.ModelSerializer.Serialize<EndScopeCommunicationMessage>(communicationMessage));
-        }
-
-        public bool Handle(ILogScope logScope, CreateLogItemRequest logRequest)
-        {
-            var rootScope = Log.RootScope;
+            var rootScope = Context.Current.Log.Root;
 
             TraceLogger.Verbose($"Handling log message for {rootScope.GetHashCode()} root scope...");
 
+            var logScope = args.LogScope;
+
             if (_outputHelperMap.TryGetValue(rootScope, out ITestOutputHelper output))
             {
+                var logRequest = args.LogMessage.ConvertToRequest();
+
                 var sharedLogMessage = new AddLogCommunicationMessage
                 {
                     ParentScopeId = logScope?.Id,
@@ -95,14 +71,45 @@ namespace ReportPortal.XUnitReporter.LogHandler
 
                 if (logRequest.Attach != null)
                 {
-                    sharedLogMessage.Attach = new Attach(logRequest.Attach.Name, logRequest.Attach.MimeType, logRequest.Attach.Data);
+                    sharedLogMessage.Attach = new Attach(logRequest.Attach.MimeType, logRequest.Attach.Data);
                 }
 
-                //output.WriteLine(Client.Converters.ModelSerializer.Serialize<AddLogCommunicationMessage>(sharedLogMessage));
                 NotifyAgent(output, Client.Converters.ModelSerializer.Serialize<AddLogCommunicationMessage>(sharedLogMessage));
             }
+        }
 
-            return true;
+        private void CommandsSource_OnEndLogScopeCommand(Shared.Execution.ILogContext logContext, Shared.Extensibility.Commands.CommandArgs.LogScopeCommandArgs args)
+        {
+            var logScope = args.LogScope;
+
+            var communicationMessage = new EndScopeCommunicationMessage
+            {
+                Id = logScope.Id,
+                EndTime = logScope.EndTime.Value,
+                Status = logScope.Status
+            };
+
+            var outputHelper = _outputHelperMap[Context.Current.Log.Root];
+
+            NotifyAgent(outputHelper, Client.Converters.ModelSerializer.Serialize<EndScopeCommunicationMessage>(communicationMessage));
+        }
+
+        private void CommandsSource_OnBeginLogScopeCommand(Shared.Execution.ILogContext logContext, Shared.Extensibility.Commands.CommandArgs.LogScopeCommandArgs args)
+        {
+            var logScope = args.LogScope;
+
+            if (_outputHelperMap.TryGetValue(Context.Current.Log.Root, out ITestOutputHelper output))
+            {
+                var communicationMessage = new BeginScopeCommunicationMessage
+                {
+                    Id = logScope.Id,
+                    ParentScopeId = logScope.Parent?.Id,
+                    Name = logScope.Name,
+                    BeginTime = logScope.BeginTime
+                };
+
+                NotifyAgent(output, Client.Converters.ModelSerializer.Serialize<BeginScopeCommunicationMessage>(communicationMessage));
+            }
         }
 
         private void NotifyAgent(ITestOutputHelper outputHelper, string serializedMessage)
